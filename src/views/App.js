245a0f24f09fb2596082b9837693f9c66bdf59fa
duykhanh21052003 <khanhnguyenduy2105@gmail.com>
2023-09-12 22:32:11 +0700

    lesson 1

--- src/views/App.js
+++ src/views/App.js
@@ -1,5 +1,5 @@
 import logo from './logo.svg';
-import './App.css';
+import './App.scss';
 
 function App() {
   return (
@@ -18,6 +18,7 @@ function App() {
           Learn React
         </a>
       </header>
+      <footer></footer>
     </div>
   );
 }
